44390715f752cddb45143af88e422e343cba2e42
Stephen <stephen.akinola@bigcabal.com>
2025-06-10 00:26:44 +0100

    fix: fix phpcs fail due ci svn issue

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -104,12 +104,13 @@ install_test_suite() {
 	if [ ! -d $WP_TESTS_DIR ]; then
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
+		git clone --depth=1 --branch=$WP_TESTS_TAG https://github.com/WordPress/wordpress-develop.git $TMPDIR/wordpress-develop
+		cp -r $TMPDIR/wordpress-develop/tests/phpunit/includes/ $WP_TESTS_DIR/
+		cp -r $TMPDIR/wordpress-develop/tests/phpunit/data/ $WP_TESTS_DIR/
 	fi
 
 	if [ ! -f wp-tests-config.php ]; then
-		download https://develop.svn.wordpress.org/${WP_TESTS_TAG}/wp-tests-config-sample.php "$WP_TESTS_DIR"/wp-tests-config.php
+		cp $TMPDIR/wordpress-develop/wp-tests-config-sample.php "$WP_TESTS_DIR"/wp-tests-config.php
 		# remove all forward slashes in the end
 		WP_CORE_DIR=$(echo $WP_CORE_DIR | sed "s:/\+$::")
 		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR"/wp-tests-config.php
